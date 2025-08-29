3e5faa6c76a9de6e3316bab14b51bc360dbcc3b1
kriti-1-9 <dwivedikriti700@gmail.com>
2025-08-29 14:04:49 +0530

    added readme and other models

---
+++ dashboard.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:102be6baa30b272b1e4e93aef46d51e6f1bb0a6f985545a9e1bb5c8802a8765b
+size 1469
---
+++ train_model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:096c8e7d9489a5efe93fadb58cae77afe6c030a076304c60e904f113c9806d0a
+size 1598
